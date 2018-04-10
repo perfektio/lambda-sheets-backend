7997c036c7fcdf61c7da53eb032ed8ee2d757791
Jonne Huotari <jonne@huotari.me>
2018-04-10 18:24:16 +0300

    Second commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let request = require("request");
+
+exports.handler = function(event, context, callback) {
+  let key = "YOUR-KEY-HERE";
+  let url = "https://spreadsheets.google.com/feeds/list/"+key+"/od6/public/values?alt=json";
+
+  request({
+    json: true,
+    url: url
+  }, function (error, response, body) {
+    if (error || response.statusCode !== 200) return
+
+    let parsed = body.feed.entry.map( (entry) => {
+      let columns = {
+        "updated": entry.updated["$t"]
+      }
+
+      // Dynamically add all relevant columns from the Sheets to the response
+      Object.keys( entry ).forEach( (key) => {
+        if ( /gsx\$/.test(key) ) {
+          let newKey = key.replace("gsx$", "");
+          columns[newKey] = entry[key]["$t"];
+        }
+      });
+
+      return columns;
+    })
+
+    callback(null, parsed);
+  });
+};
